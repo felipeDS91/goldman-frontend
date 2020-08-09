80c3777b7731ab28e0082ecfe486e2a12a8b4b8f
Felipe.DS <Felipe.DS@outlook.com>
2020-08-09 11:36:10 -0300

    fix: removes duplicate address from delivery

--- src/pages/Form/Order/OrderDelivery/index.js
+++ src/pages/Form/Order/OrderDelivery/index.js
@@ -141,7 +141,6 @@ export default function OrderDelivery({ formRef }) {
                 </Button>
               </Column>
             </Row>
-            <Address />
             <Address prefix="delivery_" />
           </>
         )}
